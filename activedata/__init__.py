592121735df4b636d0cc38db195732de6dcf16c8
tjdwill <tjdwill.gh@gmail.com>
2024-05-25 19:49:28 -0500

    Add license notices

--- activedata/__init__.py
+++ activedata/__init__.py
@@ -1,3 +1,15 @@
+#------------------------------------------------------------------------------
+# This file is part of ActiveData. ActiveData is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License v2.0 as published by the Free Software Foundation.
+#
+# ActiveData is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# ActiveData. If not, see <https://www.gnu.org/licenses/>. 
+#------------------------------------------------------------------------------
 import hy
 from activedata.array import ActiveArray
 
--- tests/access.hy
+++ tests/access.hy
@@ -1,3 +1,15 @@
+#[COPYRIGHT[
+    This file is part of ActiveData. ActiveData is free software: you can
+    redistribute it and/or modify it under the terms of the GNU General Public
+    License v2.0 as published by the Free Software Foundation.
+
+    ActiveData is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+    A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with
+    ActiveData. If not, see <https://www.gnu.org/licenses/>. 
+]COPYRIGHT]
 "
 @author: tjdwill
 @date: 21 March 2022
